3f1edb474e47698699d1a8ada3627b25958e55a1
404CoderM <117585798+404CoderM@users.noreply.github.com>
2023-04-26 19:36:54 +0530

    variable named

--- ClassroomTimeTableManagementSystem/src/classes/LoginValidate.java
+++ ClassroomTimeTableManagementSystem/src/classes/LoginValidate.java
@@ -17,51 +17,57 @@ import javax.swing.JOptionPane;
  */
 public class LoginValidate {
           
+         
           public boolean loginValidation(String username, String password) {
+                    
+           boolean valid = false;
+           boolean isFilled = false;
+           
           // check if the username and password are empty
           if ( username.isEmpty() || password.isEmpty() ){
-                    JOptionPane.showMessageDialog(null, "Please enter your name and password");
+                    //JOptionPane.showMessageDialog(null, "Please enter your name and password");
+                    isFilled = true;
           }
           
           //validate the user
           Connection conn = null;
           PreparedStatement stmt = null;
           ResultSet rs = null;
-          boolean valid = false;
 
-          try {
-                    conn = DataBaseConnection.getCon();
-                    stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
-                    stmt.setString(1, username);
-                    stmt.setString(2, password);
-                    rs = stmt.executeQuery();
-                    
-                    if ( rs.next() ){
-                              valid = true;
-                    }
-                    
-          }catch ( SQLException e ){
-                    System.out.println("Error executing query!!");           
-          } finally {
-          try {
-            if (rs != null) {
-                rs.close();
-            }
-            if (stmt != null) {
-                stmt.close();
-            }
-            if (conn != null) {
-                conn.close();
-            }
-          } catch (SQLException e) {
-            System.out.println("Error closing resources: " + e.getMessage());
-        }
-    }
+          if (!isFilled){
+                              try {
+                                        conn = DataBaseConnection.getCon();
+                                        stmt = conn.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+                                        stmt.setString(1, username);
+                                        stmt.setString(2, password);
+                                        rs = stmt.executeQuery();
 
-    if (!valid) {
-        JOptionPane.showMessageDialog(null, "Invalid username or password.");
-    }
+                                        if ( rs.next() ){
+                                                  valid = true;
+                                        }
 
+                              }catch ( SQLException e ){
+                                        System.out.println("Error executing query!!");           
+                              } finally {
+                              try {
+                                if (rs != null) {
+                                    rs.close();
+                                }
+                                if (stmt != null) {
+                                    stmt.close();
+                                }
+                                if (conn != null) {
+                                    conn.close();
+                                }
+                              } catch (SQLException e) {
+                                System.out.println("Error closing resources: " + e.getMessage());
+                            }
+                        }
+
+                        if (!valid) {
+                          //JOptionPane.showMessageDialog(null, "Invalid username or password.");
+                        }
+          }
     return valid;
 }
 
